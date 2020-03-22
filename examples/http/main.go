56cb623b4bfc32e288381a902b5a9e82bbd13e3b
Renning Bruns <nullren@github.com>
2020-03-22 15:29:03 -0700

    use gin

--- examples/http/main.go
+++ examples/http/main.go
@@ -4,14 +4,21 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gin-gonic/gin"
+
 	"github.com/nullren/go-webusers/pkg/webusers"
 )
 
 func main() {
-	http.HandleFunc("/signup", webusers.Signup)
-	http.HandleFunc("/login", webusers.Login)
-	http.HandleFunc("/settings", webusers.Settings)
+	r := gin.Default()
 
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	r.GET("/signup", gin.WrapF(webusers.Signup))
+	r.POST("/signup", gin.WrapF(webusers.Signup))
+
+	r.GET("/login", gin.WrapF(webusers.Login))
+	r.POST("/login", gin.WrapF(webusers.Login))
+
+	r.GET("/settings", gin.WrapF(webusers.Settings))
+
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
-
